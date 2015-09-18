df1ca275f8e9a59581c626bc3d30f41d54c7e878
Alejandro Medina <5almero5@gmail.com>
2015-09-18 09:52:52 -0500

    intento de optimizacion de estilos e integracion de iconos

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,10 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
-
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.styles([
+        'normalize.css',
+        'styles.css',
+        'styleicon.css'
+    ]).version('public/css/all.css');
 });
