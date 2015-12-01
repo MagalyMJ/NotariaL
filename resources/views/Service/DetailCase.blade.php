38dce1aac6c5018435b27857b31fe0c9d9618922
Alejandro Medina <5almero5@gmail.com>
2015-12-01 14:31:00 -0600

    ordenamiento de blockes en detalles de servicio

--- resources/views/Service/DetailCase.blade.php
+++ resources/views/Service/DetailCase.blade.php
@@ -12,7 +12,7 @@
 
 		<section class="caseDetail" >
 
-			<div id="partisipans_thisCase" class="Detail-participants " >
+			<div id="partisipans_thisCase" class="Detail_participants" >
 			<h3>Participantes</h3>
 			@foreach ($ServiceCase->customer as $customerSelect)
     			<div>
@@ -31,64 +31,65 @@
 				<a class="input budget-button button_normal"  href="{{route('Select_customer_InExisting_Case',$ServiceCase->id) }}">+ Participantes</a> 		   
 			</section>
 		</section>
-		<section class="caseDetail" >
-			<div id="thisCase_Data" class="Detail-general " >
-			
-				<h3>Detalles de caso </h3>
-				<div class="flex-container">
-					<div>
-						<p class="text-center"><strong> Detalle de Servicio:</strong>  {{ $ServiceCase->service_detail}} </p>		
-						<p class="text-center"><strong> Lugar:</strong>  {{ $ServiceCase->place }} </p>
-						<p class="text-center"><strong> Progreso:</strong>  %{{ $ServiceCase->progress}} </p>	
-					</div>
-					<div>
-						<p class="text-center"><strong> Avisos:</strong>  {{ $ServiceCase->notices}} - Dias Transcurridos: {{$ServiceCase->diffDateNotices()}}</p>				
-						<p class="text-center"><strong> Firmado:</strong>  {{$ServiceCase->signature}} </p>
-						<p class="text-center observation"><strong> Observaciones:</strong>  {{ $ServiceCase->observations}} </p>	
-					</div>
-				</div>
-			</div>
 
-			<section class = "action_buttons">
-				<a class="input budget-button button_normal"  href="{{route('Edit_Case_path',$ServiceCase->id) }}"> Editar </a> 		   
-			</section>
-		</section>
-
-		<section class="caseDetail" >
-			<div id="budget_thisCase" class="Detail-budeget " >
-			
-				<h3>Datos generales de presupuesto</h3>
-				<div class="flex-container">
-					<div>
-						<p class="text-center"><strong>Honorarios:</strong> ${{ $ServiceCase->budget->fee }} </p>		
-						<p class="text-center"><strong>Aprovado:</strong>  {{ $ServiceCase->budget->approved }} </p>		
-						<p class="text-center"><strong>Facturado:</strong>  {{ $ServiceCase->budget->invoiced }} </p>		
-						<p class="text-center"><strong>IVA:</strong>  ${{ $ServiceCase->budget->iva }} </p>		
-						<p class="text-center"><strong>Tipo de Pago:</strong>  {{ $ServiceCase->budget->payment_type}} </p>		
-					</div>
-					<div>
-						<p class="text-center"><strong>Total:</strong>  ${{ $ServiceCase->budget->total}} </p>		
-						<p class="text-center"><strong>Descuento de Honorarios:</strong>  $ {{ $ServiceCase->budget->discount}} </p>		
-						<p class="text-center"><strong>Gastos de Viaje:</strong>  $ {{ $ServiceCase->budget->travel_expenses}} </p>		
-						<p class="text-center"><strong>Varios:</strong>  ${{ $ServiceCase->budget->miscellaneous_expense}} </p>		
-						<p class="text-center"><strong>Anticipo:</strong>  $ {{ $ServiceCase->budget->advance_payment}} </p>
-					</div>		
-				</div>
-			
-
-			</div>
-			
-			<section class = "action_buttons">
-
-				<a class="input budget-button button_normal"  href="{{route('PdfBuget',$ServiceCase->budget->id) }}" target="_blank">PDF</a>
+		<div>
+			<section class="caseDetail " >
+				<div id="thisCase_Data" class="Detail_general " >
 				
-				<a class="input budget-button button_big"  href="{{route('EditBudget',$ServiceCase->budget->id) }}">Crear Presupuesto</a> 
-						   
-				<a class="input budget-button button_normal"  href="{{route('Case_Payments',$ServiceCase->id) }}"> Pagos </a> 		   
-		
-			</section>
-		</section>
+					<h3>Detalles de caso </h3>
+					<div class="flex-container">
+						<div>
+							<p class="text-center"><strong> Detalle de Servicio:</strong>  {{ $ServiceCase->service_detail}} </p>		
+							<p class="text-center"><strong> Lugar:</strong>  {{ $ServiceCase->place }} </p>
+							<p class="text-center"><strong> Progreso:</strong>  %{{ $ServiceCase->progress}} </p>	
+						</div>
+						<div>
+							<p class="text-center"><strong> Avisos:</strong>  {{ $ServiceCase->notices}} - Dias Transcurridos: {{$ServiceCase->diffDateNotices()}}</p>				
+							<p class="text-center"><strong> Firmado:</strong>  {{$ServiceCase->signature}} </p>
+							<p class="text-center observation"><strong> Observaciones:</strong>  {{ $ServiceCase->observations}} </p>	
+						</div>
+					</div>
+					<section class = "action_buttons">
+						<a class="input budget-button button_normal"  href="{{route('Edit_Case_path',$ServiceCase->id) }}"> Editar </a> 		   
+					</section>
+				</div>
 
+			
+			</section>
+
+			<section class="caseDetail" >
+				<div id="budget_thisCase" class="Detail_budeget" >
+				
+					<h3>Datos generales de presupuesto</h3>
+					<div class="flex-container">
+						<div>
+							<p class="text-center"><strong>Honorarios:</strong> ${{ $ServiceCase->budget->fee }} </p>		
+							<p class="text-center"><strong>Aprovado:</strong>  {{ $ServiceCase->budget->approved }} </p>		
+							<p class="text-center"><strong>Facturado:</strong>  {{ $ServiceCase->budget->invoiced }} </p>		
+							<p class="text-center"><strong>IVA:</strong>  ${{ $ServiceCase->budget->iva }} </p>		
+							<p class="text-center"><strong>Tipo de Pago:</strong>  {{ $ServiceCase->budget->payment_type}} </p>		
+						</div>
+						<div>
+							<p class="text-center"><strong>Total:</strong>  ${{ $ServiceCase->budget->total}} </p>		
+							<p class="text-center"><strong>Descuento de Honorarios:</strong>  $ {{ $ServiceCase->budget->discount}} </p>		
+							<p class="text-center"><strong>Gastos de Viaje:</strong>  $ {{ $ServiceCase->budget->travel_expenses}} </p>		
+							<p class="text-center"><strong>Varios:</strong>  ${{ $ServiceCase->budget->miscellaneous_expense}} </p>		
+							<p class="text-center"><strong>Anticipo:</strong>  $ {{ $ServiceCase->budget->advance_payment}} </p>
+						</div>		
+					</div>
+				
+					<section class = "action_buttons">
+
+						<a class="input budget-button button_normal"  href="{{route('PdfBuget',$ServiceCase->budget->id) }}" target="_blank">PDF</a>
+						
+						<a class="input budget-button button_big"  href="{{route('EditBudget',$ServiceCase->budget->id) }}">Crear Presupuesto</a> 
+							   
+						<a class="input budget-button button_normal"  href="{{route('Case_Payments',$ServiceCase->id) }}"> Pagos </a> 		   
+					</section>
+
+				</div>	
+			</section>
+		</div>
 
 
 	</div>
