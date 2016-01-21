e87f12e78079b08255847889a9142a7e6d20418c
Alejandro Medina <5almero5@gmail.com>
2016-01-21 11:13:48 -0600

    completado de usuarios y nuevo usuario en los seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -32,11 +32,11 @@ class UsersTableSeeder extends Seeder
         $user->save();
 
         $user = new User();
-        $user->name = "Ivan";
-        $user->fathers_last_name = "";
-        $user->mothers_last_name = "";
+        $user->name = "Herminio Ivan";
+        $user->fathers_last_name = "Ventura";
+        $user->mothers_last_name = "Moreno";
         $user->user_name= "ivan07";
-        $user->email="ivan@gmail.com";
+        $user->email="ivanvent@hotmail.com";
         $user->password = bcrypt('90Ntrstd');
         $user->remember_token = str_random(10);
         $user->save();
@@ -44,9 +44,9 @@ class UsersTableSeeder extends Seeder
         $user->save();
 
         $user = new User();
-        $user->name = "Joel";
-        $user->fathers_last_name = "";
-        $user->mothers_last_name = "";
+        $user->name = "Joel Enrique";
+        $user->fathers_last_name = "Montoya";
+        $user->mothers_last_name = "García";
         $user->user_name= "joel04";
         $user->email="joel@gmail.com";
         $user->password = bcrypt('7haVr5fr');
@@ -56,11 +56,11 @@ class UsersTableSeeder extends Seeder
         $user->save();
 
         $user = new User();
-        $user->name = "Taide";
-        $user->fathers_last_name = "";
-        $user->mothers_last_name = "";
+        $user->name = "Laura Tayde";
+        $user->fathers_last_name = "Ventura";
+        $user->mothers_last_name = "López";
         $user->user_name= "taide10";
-        $user->email="taide@gmail.com";
+        $user->email="taideventura@hotmail.com";
         $user->password = bcrypt('6grYfa8r');
         $user->remember_token = str_random(10);
         $user->save();
@@ -69,10 +69,10 @@ class UsersTableSeeder extends Seeder
 
         $user = new User();
         $user->name = "Juan Carlos";
-        $user->fathers_last_name = "";
-        $user->mothers_last_name = "";
+        $user->fathers_last_name = "de Luna";
+        $user->mothers_last_name = "Esquivel";
         $user->user_name= "juanC06";
-        $user->email="juan@gmail.com";
+        $user->email="juandelunae79@gmail.com";
         $user->password = bcrypt('3hrDf28r');
         $user->remember_token = str_random(10);
         $user->save();
@@ -99,6 +99,18 @@ class UsersTableSeeder extends Seeder
         $user->user_name = "user";
         $user->email="user@gmail.com";
         $user->password = bcrypt('123456789');
+        $user->remember_token = str_random(10);
+        $user->save();
+        $user->attachRole(3);
+        $user->save();
+
+        $user = new User();
+        $user->name = "Veronica Elisabeth";
+        $user->fathers_last_name = "Barba";
+        $user->mothers_last_name = "Perez";
+        $user->user_name = "vero05";
+        $user->email="veronica.lizi@hotmail.com";
+        $user->password = bcrypt('5shr6HrT');
         $user->remember_token = str_random(10);
         $user->save();
         $user->attachRole(3);
